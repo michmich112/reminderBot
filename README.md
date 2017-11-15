a1be22b58037d3db780433dbbbd1247133531465
michmich112 <mchl.aauee.98@gmail.com>
2017-11-15 14:46:56 -0500

    started by modifying google's quickstart and making sure i can send an email with it

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # reminderBot [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) [![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/dwyl/esta/issues)
+
 Automatically send e-mail based on info provided by an airtable base. In this code, if an item is *OVERDUE* send an e-mail reminding the individual to renew the lease or bring it back to the lab.
+
 Uses Node.js and the following APIs: Gmail API [![npm version](https://badge.fury.io/js/googleapis.svg)](https://badge.fury.io/js/googleapis), Airtable API [![npm version](https://badge.fury.io/js/airtable.svg)](https://badge.fury.io/js/airtable)
 
 ## Installation
-Requires Node.js >= 0.10.0 and npm [![npm version](https://badge.fury.io/js/npm.svg)](https://badge.fury.io/js/npm) .
+
+Requires Node.js >= 0.10.0 and npm [![npm version](https://badge.fury.io/js/npm.svg)](https://badge.fury.io/js/npm).
+
 To install run:
 
 ### On Debian systems (ubuntu)
@@ -12,13 +16,17 @@ To install run:
 ### On Darwin (Mac OSX with Homebrew installed)
 `$ brew install node`
 
-Once installed run `node -v` (or `nodejs -v`) and `npm -v` to see check if the packages are correctly installed on your machine.
+Once installed run `node -v` and `npm -v` to see check if the packages are correctly installed on your machine.
+
 Then run:
+
 ```
 $ cd reminderBot/
 $ npm start
 ```
+
 and the service will start up automatically
 
 ## Configuration
+
 Configuration will come once the service is finished
--- bot.js
+++ bot.js
@@ -1,6 +1,127 @@
 var app_info        = require('./package.json');        //package to make module available on npm
 var fs              = require('fs');                    //give access to files to avoid posting your API key and client-secret (google)
+var readline        = require('readline');              //needed for google-Auth
 var google          = require('googleapis');            //google api module
 var gooogleAuth     = require('google-auth-library');   //google authentication module to use your own gmail account
 var Airtable        = require('airtable');              //airtable api to access the database
 
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/gmail-nodejs-reminderBot.json
+var SCOPES = ['https://mail.google.com/','https://www.googleapis.com/auth/gmail.modify','https://www.googleapis.com/auth/gmail.compose','https://www.googleapis.com/auth/gmail.send'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-reminderBot.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    console.log('Make sure you downloaded the correct secret file and you renamed it to client_secret.json');
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Gmail API.
+  authorize(JSON.parse(content), listLabels);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  var gmail = google.gmail('v1');
+  gmail.users.labels.list({
+    auth: auth,
+    userId: 'me',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var labels = response.labels;
+    if (labels.length == 0) {
+      console.log('No labels found.');
+    } else {
+      console.log('Labels:');
+      for (var i = 0; i < labels.length; i++) {
+        var label = labels[i];
+        console.log('- %s', label.name);
+      }
+    }
+  });
+}
