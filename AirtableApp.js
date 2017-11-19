e192662834eab41419de64a7de7b8d57fbcb7848
michmich112 <mchl.aauee.98@gmail.com>
2017-11-18 21:09:18 -0500

    FINALY got the Aitable API thingy to work. Yay I guess. Stilol need top finish and clean it)

--- AirtableApp.js
+++ AirtableApp.js
@@ -1,12 +1,12 @@
 var Airtable = require('airtable');
 var fs = require('fs');
-
+var base = undefined;
 fs.readFile('AirtableAPIkey.key', function conf(err,key){
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
         apiKey: key
     });
-    var base = Airtable.base('appGh7ESCOFPw5h8R');
+    base = Airtable.base('appGh7ESCOFPw5h8R');
     
     base('Rental Sign Out').select({
         // Selecting the first 3 records in Main View:
@@ -22,27 +22,54 @@ function test1(base,records,callback){
     records.forEach(function(record) {
         if(record.get('Due?')=="⏰OVERDUE⏰"){
             try{
-                people.push(record.get('Name'));
+                people.push({Name:record.get('Name'),Due: record.get('Due Date'),Object: record.get('Equipment Rented')});
             }catch(err){
                 console.error(err);
             }
         }
     });
+    //console.log(people);
     callback(base,people);
 }
 
 function sendMessages(base,names){ //returns {to: string, message: string}
     //will need name,due_date, object(s) as parameters for a complete e-mail
+    //console.log('hi');
     names.forEach(function(value){
-        var person = base('Members').find(value,(function(err, record){
-            if (err) { return err;}
+        console.log(value);
+        var person = base('Members').find(value.Name,(function(err, record){
+            if (err) { console.error(err)}
             //console.log("in");
-            message = makeMessage({
-                name: record.get("Name"),
-                email: record.get("Email")
-            });
-            console.log(message);
+            value.Name = record.get('Name');
+            value.Email = record.get('Email');
+            make(base,value);
     }));});
+}
+
+function getObjects(base,data){
+    for(var i=0;i<data.Objects.length;i++){
+        //need to make sure that the callback is executed first
+    }
+}
+
+function make(base,data){
+    var partA = "Hi " + data.Name + "this is The Factory. The Following item(s) that you have rented are now overdue: ";
+    var partB = " (due on " + data.Due + " ). Please return it or reply to this message if you want to continue using it.\nNote that due to limited stock we may ask you to bring it back regardless if other people wish to use it.\nBest,\nThe Factory Management Team";
+    //console.log(partA);
+    //console.log(data.Object);
+    construct(partA,partB,data.Object);
+}
+
+function construct(partA,partB,objs){
+    if(objs.length == 0){
+        console.log(partA + partB);
+    }else{
+        //console.log(objs.shift());
+        base('Rental Inventory').find(objs.shift(),function(err, object){
+            //console.log(obj);
+            construct(partA+", "+object.get('Name'),partB,objs);
+        })
+    }
 }
 
 function makeMessage(data){ //didnt need to create a function but makes this a bit more readable
